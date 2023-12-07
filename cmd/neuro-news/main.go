790d601d31a3fa1e9f6b6ade512a4d7d97825d9a
alekslesik <alekslesik@gmail.com>
2023-12-07 16:32:56 +0300

    test #5

---
+++ cmd/neuro-news/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello")
+	})
+
+	var server = http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+		ReadTimeout: 5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+
+	}
+
+	server.ListenAndServe()
+
+}
