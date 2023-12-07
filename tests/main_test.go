f69b3f1b57595511c733f8675c1525340c994dba
alekslesik <alekslesik@gmail.com>
2023-12-07 16:25:08 +0300

    tets actions

---
+++ tests/main_test.go
@@ -0,0 +1,13 @@
+// tests/main_test.go
+package tests
+
+import (
+	"testing"
+
+	// "githhub.com/alekslesik/neuro-news/cmd/neuro-news" // Замените на путь к вашему пакету
+)
+
+func TestHelloHandler(t *testing.T) {
+
+}
+
