1b91c29fdff989dd19010b6d86a10e9668b2c1a6
alekslesik <alekslesik@gmail.com>
2023-12-08 16:58:41 +0300

    add first function

--- cmd/neuro-news/main.go
+++ cmd/neuro-news/main.go
@@ -1,26 +1,48 @@
+// cmd/neuro-news/main.go
+
 package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"time"
+
+	"githhub.com/alekslesik/neuro-news/internal/app/handler"
+	"githhub.com/alekslesik/neuro-news/internal/config"
+	"githhub.com/alekslesik/neuro-news/internal/infra/logger"
+	"githhub.com/alekslesik/neuro-news/internal/web/router"
 )
 
 func main() {
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello stovpiaga")
-	})
-
-	var server = http.Server{
-		Addr:    ":8080",
-		Handler: mux,
-		ReadTimeout: 5 * time.Second,
-		WriteTimeout: 5 * time.Second,
-
+	// Load configuration
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal("Error loading configuration:", err)
 	}
 
-	server.ListenAndServe()
+	// Initialize logger
+	log, err := logger.New(cfg.LogLevel)
+	if err != nil {
+		log.Fatal("Error initializing logger:", err)
+	}
 
+	// Create HTTP router
+	r := router.New()
+
+	// Set up HTTP routes
+	handler.SetupRoutes(r)
+
+	// Start HTTP server
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.Port),
+		Handler:      r,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	log.Printf("Starting Neuro News application on :%d...\n", cfg.Port)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal("Error starting server:", err)
+	}
 }
