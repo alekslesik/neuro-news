afda762d6e38d431dd8ddee1f78a5d4301d7ea20
alekslesik <alekslesik@gmail.com>
2024-04-09 15:15:44 +0300

    upd go to 1.22.2 and refactor mux

--- internal/app/handler/handler_article.go
+++ internal/app/handler/handler_article.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 
@@ -22,8 +23,8 @@ func NewArticleHandler(appHandler *AppHandler, l *logger.Logger) *ArticleHandler
 	}
 }
 
-// GetHomeArticles GET handler for home page /?PAGEN_1
-func (a *ArticleHandler) GetHomeArticles(w http.ResponseWriter, r *http.Request) {
+// Home GET handler for home page with pagination: GET /?PAGEN_1
+func (a *ArticleHandler) Home(w http.ResponseWriter, r *http.Request) {
 	const (
 		op       = "GetHomeArticles()"
 		tmplFile = "home.page.html"
@@ -33,6 +34,9 @@ func (a *ArticleHandler) GetHomeArticles(w http.ResponseWriter, r *http.Request)
 		td  *template.TemplateData
 		err error
 	)
+
+	id := r.PathValue("id")
+	fmt.Println(id)
 
 	page := r.URL.Query().Get("PAGEN_1")
 
@@ -47,23 +51,62 @@ func (a *ArticleHandler) GetHomeArticles(w http.ResponseWriter, r *http.Request)
 	}
 }
 
-// GetArticle GET handler for article page /news/#article_href
-func (a *ArticleHandler) GetArticle(w http.ResponseWriter, r *http.Request) {
+// Category GET handler for category page with pagination: GET /{category}?PAGEN_1
+func (a *ArticleHandler) Category(w http.ResponseWriter, r *http.Request) {
+	const (
+		op       = "GetArticleList()"
+		tmplFile = "article.list.page.html"
+	)
+
+	var (
+		td  *template.TemplateData
+		err error
+	)
+
+	// get article URL
+	urlParts := strings.Split(r.URL.Path, "/")
+
+	if len(urlParts) < 3 {
+		http.NotFound(w, r)
+		return
+	}
+
+	url := strings.Trim(strings.Trim(urlParts[len(urlParts)-1], "\""), " ")
+
+	td, err = a.AppHandler.articleService.GetArticleTemplateData(url)
+	if err != nil {
+		a.l.Error().Msgf("%s: GetArticle error > %s", op, err)
+	}
+
+	err = a.AppHandler.articleService.RenderTemplate(w, r, tmplFile, td)
+	if err != nil {
+		a.l.Error().Msgf("%s: RenderTemplate article page error > %s", op, err)
+	}
+}
+
+// Article GET handler for article page: GET /{category}/{article}
+func (a *ArticleHandler) Article(w http.ResponseWriter, r *http.Request) {
 	const (
 		op       = "GetArticle()"
 		tmplFile = "article.page.html"
 	)
 
+	var (
+		td  *template.TemplateData
+		err error
+	)
+
 	// get article URL
 	urlParts := strings.Split(r.URL.Path, "/")
-	if len(urlParts) < 2 {
+
+	if len(urlParts) < 3 {
 		http.NotFound(w, r)
 		return
 	}
 
-	url := urlParts[len(urlParts)-1]
+	url := strings.Trim(strings.Trim(urlParts[len(urlParts)-1], "\""), " ")
 
-	td, err := a.AppHandler.articleService.GetArticleTemplateData(url)
+	td, err = a.AppHandler.articleService.GetArticleTemplateData(url)
 	if err != nil {
 		a.l.Error().Msgf("%s: GetArticle error > %s", op, err)
 	}
--- internal/pkg/router/router.go
+++ internal/pkg/router/router.go
@@ -21,12 +21,11 @@ func (r *Router) Route() http.Handler {
 	// logger and recovery (crash-free) middleware
 	mux := http.NewServeMux()
 
-	// engine := gin.New()
+	// dynamic pages
+	mux.HandleFunc("/{$}", r.h.ArticleHandler.Home)
 
-	// engine.GET("/", r.h.ArticleHandler.GetAllArticles)
-
-	mux.HandleFunc("/", r.h.ArticleHandler.GetHomeArticles)
-	mux.HandleFunc("/news/", r.h.ArticleHandler.GetArticle)
+	mux.HandleFunc("/news/{category}/{article}/", r.h.ArticleHandler.Article)
+	mux.HandleFunc("/news/{category}/", r.h.ArticleHandler.Category)
 
 	// static pages
 	mux.HandleFunc("/about", r.h.CommonHandler.GetAboutPage)
@@ -34,28 +33,9 @@ func (r *Router) Route() http.Handler {
 	mux.HandleFunc("/advertisement", r.h.CommonHandler.GetAdvertisementPage)
 	mux.HandleFunc("/privacy", r.h.CommonHandler.GetPrivacyPage)
 
-	// router.POST("/somePost", posting)
-	// router.PUT("/somePut", putting)
-	// router.DELETE("/someDelete", deleting)
-	// router.PATCH("/somePatch", patching)
-	// router.HEAD("/someHead", head)
-	// router.OPTIONS("/someOptions", options)
-
-	// Пример использования функции Dir
-	// В данном случае, мы указываем путь к директории "static", которая будет доступна на сервере
-	// Если listDirectory установлено в true, то файлы директории будут отображаться, иначе - нет
-	// engine.StaticFS("/static", http.Dir("./"))
-
 	// file server for static files
 	fileServer := http.FileServer(http.Dir("./website/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	// for end-to-end testing
-	// mux.Get("/ping", http.HandlerFunc(ping))
-
-	// return standardMiddleware.Then(mux)
-
-	// return engine
 
 	return mux
 }
